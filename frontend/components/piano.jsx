a1b80b8257cdbd95761fff21a3f089d5764af527
Leo Salat <leo.salat@gmail.com>
2017-03-28 13:18:37 -0700

    move piano key to piano_key.jsx; add piano sound url

--- frontend/components/piano.jsx
+++ frontend/components/piano.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PianoKey from './piano_key';
 
 class Piano extends React.Component {
   constructor(props) {
@@ -6,32 +7,38 @@ class Piano extends React.Component {
   }
 
   render() {
+    const key_data = [
+      {note: "white b 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/B4v16.mp3'},
+      {note: "black as 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A%234v16.mp3'},
+      {note: "white a 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A4v16.mp3'},
+      {note: "black gs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G%234v16.mp3'},
+      {note: "white g 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G4v16.mp3'},
+      {note: "black fs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F%234v16.mp3'},
+      {note: "white f 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F4v16.mp3'},
+      {note: "white e 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/E4v16.mp3'},
+      {note: "black ds 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D%234v16.mp3'},
+      {note: "white d 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D4v16.mp3'},
+      {note: "black cs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C%234v16.mp3'},
+      {note: "white c 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C4v16.mp3'},
+      {note: "white b 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/B3v16.mp3'},
+      {note: "black as 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A%233v16.mp3'},
+      {note: "white a 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A3v16.mp3'},
+      {note: "black gs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G%233v16.mp3'},
+      {note: "white g 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G3v16.mp3'},
+      {note: "black fs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F%233v16.mp3'},
+      {note: "white f 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F3v16.mp3'},
+      {note: "white e 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/E3v16.mp3'},
+      {note: "black ds 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D%233v16.mp3'},
+      {note: "white d 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D3v16.mp3'},
+      {note: "black cs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C3%23v16.mp3'},
+      {note: "white c 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C3v16.mp3'}
+    ]
+
+    const notes = key_data.map(({note, src}) => <PianoKey note={note} src={src} key={note} />);
+
     return (
       <ul className="piano">
-        <li className="white b"></li>
-        <li className="black as"></li>
-        <li className="white a"></li>
-        <li className="black gs"></li>
-        <li className="white g"></li>
-        <li className="black fs"></li>
-        <li className="white f"></li>
-        <li className="white e"></li>
-      	<li className="black ds"></li>
-    	  <li className="white d"></li>
-    	  <li className="black cs"></li>
-    	  <li className="white c"></li>
-        <li className="white b"></li>
-        <li className="black as"></li>
-        <li className="white a"></li>
-        <li className="black gs"></li>
-        <li className="white g"></li>
-        <li className="black fs"></li>
-        <li className="white f"></li>
-        <li className="white e"></li>
-      	<li className="black ds"></li>
-    	  <li className="white d"></li>
-    	  <li className="black cs"></li>
-    	  <li className="white c"></li>
+        {notes}
       </ul>
     );
   }
