26085e1cdfdd464131868805b8450459000f848a
Leo Salat <leo.salat@gmail.com>
2017-03-28 09:52:00 -0700

    add additional keys

--- frontend/components/piano.jsx
+++ frontend/components/piano.jsx
@@ -20,6 +20,18 @@ class Piano extends React.Component {
     	  <li className="white d"></li>
     	  <li className="black cs"></li>
     	  <li className="white c"></li>
+        <li className="white b"></li>
+        <li className="black as"></li>
+        <li className="white a"></li>
+        <li className="black gs"></li>
+        <li className="white g"></li>
+        <li className="black fs"></li>
+        <li className="white f"></li>
+        <li className="white e"></li>
+      	<li className="black ds"></li>
+    	  <li className="white d"></li>
+    	  <li className="black cs"></li>
+    	  <li className="white c"></li>
       </ul>
     );
   }
