426b4d569c5a3cd511ca53458e55cda3e90480b1
Leo Salat <leo.salat@gmail.com>
2017-03-29 10:57:04 -0700

    add default state; export configureStore

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,11 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (state = preloadedState) => (
+const configureStore = (state = {}) => (
   createStore(
     rootReducer,
     state
   )
 );
+
+export default configureStore;
