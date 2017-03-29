ae3b2c61892003a8007b1495be7834c1ae206dce
Leo Salat <leo.salat@gmail.com>
2017-03-29 10:55:15 -0700

    add piano_keys.js (list of notes and urls)

---
+++ frontend/util/piano_keys.js
@@ -0,0 +1,29 @@
+const keyData = [
+  {key: "white c 5", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C5v16.mp3'},
+  {key: "white b 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/B4v16.mp3'},
+  {key: "black as 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A%234v16.mp3'},
+  {key: "white a 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A4v16.mp3'},
+  {key: "black gs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G%234v16.mp3'},
+  {key: "white g 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G4v16.mp3'},
+  {key: "black fs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F%234v16.mp3'},
+  {key: "white f 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F4v16.mp3'},
+  {key: "white e 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/E4v16.mp3'},
+  {key: "black ds 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D%234v16.mp3'},
+  {key: "white d 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D4v16.mp3'},
+  {key: "black cs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C%234v16.mp3'},
+  {key: "white c 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C4v16.mp3'},
+  {key: "white b 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/B3v16.mp3'},
+  {key: "black as 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A%233v16.mp3'},
+  {key: "white a 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A3v16.mp3'},
+  {key: "black gs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G%233v16.mp3'},
+  {key: "white g 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G3v16.mp3'},
+  {key: "black fs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F%233v16.mp3'},
+  {key: "white f 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F3v16.mp3'},
+  {key: "white e 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/E3v16.mp3'},
+  {key: "black ds 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D%233v16.mp3'},
+  {key: "white d 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D3v16.mp3'},
+  {key: "black cs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C3%23v16.mp3'},
+  {key: "white c 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C3v16.mp3'}
+];
+
+export default keyData;
