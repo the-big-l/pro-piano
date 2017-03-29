2a97ac38052432cca8c333e977008f330df8861f
Leo Salat <leo.salat@gmail.com>
2017-03-29 10:54:31 -0700

    connect piano to store; move list of keys to util to be accessible from state

--- frontend/components/piano.jsx
+++ frontend/components/piano.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PianoKey from './piano_key';
 
 class Piano extends React.Component {
@@ -7,41 +8,18 @@ class Piano extends React.Component {
   }
 
   render() {
-    const key_data = [
-      {note: "white b 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/B4v16.mp3'},
-      {note: "black as 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A%234v16.mp3'},
-      {note: "white a 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A4v16.mp3'},
-      {note: "black gs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G%234v16.mp3'},
-      {note: "white g 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G4v16.mp3'},
-      {note: "black fs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F%234v16.mp3'},
-      {note: "white f 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F4v16.mp3'},
-      {note: "white e 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/E4v16.mp3'},
-      {note: "black ds 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D%234v16.mp3'},
-      {note: "white d 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D4v16.mp3'},
-      {note: "black cs 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C%234v16.mp3'},
-      {note: "white c 4", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C4v16.mp3'},
-      {note: "white b 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/B3v16.mp3'},
-      {note: "black as 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A%233v16.mp3'},
-      {note: "white a 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/A3v16.mp3'},
-      {note: "black gs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G%233v16.mp3'},
-      {note: "white g 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/G3v16.mp3'},
-      {note: "black fs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F%233v16.mp3'},
-      {note: "white f 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/F3v16.mp3'},
-      {note: "white e 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/E3v16.mp3'},
-      {note: "black ds 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D%233v16.mp3'},
-      {note: "white d 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/D3v16.mp3'},
-      {note: "black cs 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C3%23v16.mp3'},
-      {note: "white c 3", src: 'https://s3-us-west-1.amazonaws.com/pro-piano/assets/audio/C3v16.mp3'}
-    ]
-
-    const notes = key_data.map(({note, src}) => <PianoKey note={note} src={src} key={note} />);
+    const pianoKeys = this.props.pianoKeys.map(({key, src}) => <PianoKey pianoKey={key} src={src} key={key} />);
 
     return (
       <ul className="piano">
-        {notes}
+        {pianoKeys}
       </ul>
     );
   }
 }
 
-export default Piano;
+const mapStateToProps = ({pianoKeys}) => ({
+  pianoKeys
+});
+
+export default connect(mapStateToProps)(Piano);
