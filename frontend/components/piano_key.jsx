202a754c8abe30d61cdac425a44525b87ec75565
Leo Salat <leo.salat@gmail.com>
2017-03-29 10:56:07 -0700

    update classNames

--- frontend/components/piano_key.jsx
+++ frontend/components/piano_key.jsx
@@ -3,31 +3,27 @@ import React from 'react';
 class PianoKey extends React.Component {
   constructor(props) {
     super(props);
-    this.audio_id = props.note.replace(/\s/g,'');
+    this.audio_id = props.pianoKey.replace(/\s/g,'');
     this.mouseDown = this.mouseDown.bind(this);
-    this.dragOver = this.dragOver.bind(this);
   }
+
   componentDidMount() {
     this.sound = document.getElementById(this.audio_id);
   }
 
-  mouseDown() {
-    this.sound.load()
-    this.sound.play();
-  }
-
-  dragOver() {
+  mouseDown(e) {
+    console.log(this.refs.lane.getBoundingClientRect());
     this.sound.load()
     this.sound.play();
   }
 
   render() {
     return (
-      <div className='key'>
+      <div className='key-wrapper'>
         <li
+          ref='lane'
           onMouseDown={this.mouseDown}
-          onDragOver={this.dragOver}
-          className={this.props.note}>
+          className={this.props.pianoKey}>
         </li>
         <audio id={this.audio_id} preload='auto' src={this.props.src}></audio>
       </div>
