6ac5108c190ac4421fa6360c6c1b84e5c2d91cca
Leo Salat <leo.salat@gmail.com>
2017-03-29 20:12:05 -0700

    add note and roll reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux';
 import pianoReducer from './piano_reducer';
+import noteReducer from './note_reducer';
+import rollReducer from './roll_reducer';
 
 const rootReducer = combineReducers({
-  pianoKeys: pianoReducer
+  pianoKeys: pianoReducer,
+  computedNote: noteReducer,
+  pianoRoll: rollReducer
 });
 
 export default rootReducer;
