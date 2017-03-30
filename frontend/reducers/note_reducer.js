c0600f38eb94b5c788795e8c8b5e2f81a1011399
Leo Salat <leo.salat@gmail.com>
2017-03-29 23:09:17 -0700

    add current mouse pos and clear note cases

--- frontend/reducers/note_reducer.js
+++ frontend/reducers/note_reducer.js
@@ -1,10 +1,17 @@
-import { SET_DRAGGING, SET_PITCH, SET_START_POS, SET_END_POS } from '../actions/beat_actions';
+import {
+        SET_DRAGGING,
+        SET_PITCH,
+        SET_START_POS,
+        SET_MOUSE_POS,
+        SET_END_POS,
+        CLEAR_NOTE } from '../actions/beat_actions';
 import merge from 'lodash/merge';
 
 const _nullNote = {
   dragging: false,
   pitch: null,
   startPos: null,
+  currentPos: null,
   endPos: null
 }
 
@@ -16,8 +23,12 @@ const noteReducer = (state = _nullNote, action) => {
       return merge({}, state, {pitch: action.pitch});
     case SET_START_POS:
       return merge({}, state, {startPos: action.startPos});
+    case SET_MOUSE_POS:
+      return merge({}, state, {currentPos: action.currentPos});
     case SET_END_POS:
       return merge({}, state, {endPos: action.endPos});
+    case CLEAR_NOTE:
+      return merge({}, _nullNote);
     default:
       return state;
   }
