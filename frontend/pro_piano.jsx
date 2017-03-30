aa47e57d1e6464980c92244620cff9b2a94f9d5f
Leo Salat <leo.salat@gmail.com>
2017-03-29 20:11:33 -0700

    add store to window for debugging

--- frontend/pro_piano.jsx
+++ frontend/pro_piano.jsx
@@ -7,6 +7,7 @@ import configureStore from './store/store';
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
+  window.store = store;
 
   ReactDOM.render(<Root store={store} />, root);
 });
