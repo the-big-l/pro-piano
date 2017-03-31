b7fbaf9da5d730540ea01b04e1336c60095b2be0
Leo Salat <leo.salat@gmail.com>
2017-03-30 18:18:30 -0700

    update state to be an object of pitch arrays

--- frontend/reducers/roll_reducer.js
+++ frontend/reducers/roll_reducer.js
@@ -1,12 +1,22 @@
 import { CREATE_NOTE } from '../actions/beat_actions';
+import keyData from '../util/piano_keys';
 import merge from 'lodash/merge';
 
-const _emptyRoll = [];
+function createEmptyRoll() {
+  const _emptyRoll = {spacer: []};
+  keyData.forEach(key => {
+    _emptyRoll[key.pitch] = [];
+  })
+  return _emptyRoll;
+}
 
-const rollReducer = (state = _emptyRoll, action) => {
+const rollReducer = (state = createEmptyRoll(), action) => {
+  const newState = merge({}, state);
   switch(action.type) {
     case CREATE_NOTE:
-      return [...state, action.note];
+      newState[action.note.pitch].push(action.note);
+      console.log(newState);
+      return newState;
     default:
       return state;
   }
